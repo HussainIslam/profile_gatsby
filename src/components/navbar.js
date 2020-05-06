b504911029dd331f2773103502bf3ca7fd3ae68a
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-05-06 01:18:58 -0400

    added link to navbar brand

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,13 +6,27 @@ import navbarStyles from "./navbar.module.css"
 
 const NavBar = () => (
   <div className={navbarStyles.container}>
-    <div className={navbarStyles.brand}>I am Hussain</div>
+    <div className={navbarStyles.brandContainer}>
+      <Link to="/" className={navbarStyles.brand}>
+        I am Hussain
+      </Link>
+    </div>
     <div className={navbarStyles.unorderedlist}>
-      <Link to="/about/">About</Link>
-      <Link to="/portfolio/">Portfolio</Link>
-      <Link to="/resume/">Resume</Link>
-      <Link to="/blog/">Blog</Link>
-      <Link to="/contact/">Contact</Link>
+      <Link className={navbarStyles.listitem} to="/about/">
+        About
+      </Link>
+      <Link className={navbarStyles.listitem} to="/portfolio/">
+        Portfolio
+      </Link>
+      <Link className={navbarStyles.listitem} to="/resume/">
+        Resume
+      </Link>
+      <Link className={navbarStyles.listitem} to="/blog/">
+        Blog
+      </Link>
+      <Link className={navbarStyles.listitem} to="/contact/">
+        Contact
+      </Link>
     </div>
   </div>
 )
