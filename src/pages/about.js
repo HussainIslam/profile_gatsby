48e25628e39fa3e2339211c734e8be1bae909fcd
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-05-09 16:44:22 -0400

    converted about page to class component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,108 @@
-import React from "react"
+import React, { Component } from "react"
 
 import Layout from "../components/layout"
 
-const About = () => (
-  <Layout>
-    <h1>About</h1>
-  </Layout>
-)
+class About extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      whoAmIOpen: false,
+      whereWasIOpen: false,
+      intriguesOpen: false,
+      drivesOpen: false,
+      headingOpen: false,
+    }
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1>About</h1>
+        <div>
+          <div>
+            <h2>Who am I?</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              sapien mauris, tincidunt elementum sapien eget, interdum ultricies
+              urna. Curabitur vel quam ut odio sodales ullamcorper at eget
+              tortor. Duis ac bibendum sem, sollicitudin aliquet nisl.
+              Suspendisse fermentum metus et nulla varius, commodo luctus eros
+              imperdiet. Fusce vitae lobortis nisi. Sed accumsan aliquam purus,
+              vitae lobortis mi scelerisque et. Nullam gravida mi a ante
+              posuere, in consequat nulla semper. Suspendisse vel porta mauris.
+              Ut eu magna venenatis, rhoncus massa in, porttitor justo. Integer
+              sit amet eros dolor. Integer pulvinar ultricies augue, vitae
+              tristique neque condimentum eget. Nam nec varius odio.
+            </p>
+          </div>
+          <div>
+            <h2>Where was I?</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              sapien mauris, tincidunt elementum sapien eget, interdum ultricies
+              urna. Curabitur vel quam ut odio sodales ullamcorper at eget
+              tortor. Duis ac bibendum sem, sollicitudin aliquet nisl.
+              Suspendisse fermentum metus et nulla varius, commodo luctus eros
+              imperdiet. Fusce vitae lobortis nisi. Sed accumsan aliquam purus,
+              vitae lobortis mi scelerisque et. Nullam gravida mi a ante
+              posuere, in consequat nulla semper. Suspendisse vel porta mauris.
+              Ut eu magna venenatis, rhoncus massa in, porttitor justo. Integer
+              sit amet eros dolor. Integer pulvinar ultricies augue, vitae
+              tristique neque condimentum eget. Nam nec varius odio.
+            </p>
+          </div>
+          <div>
+            <h2>What intrigues me?</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              sapien mauris, tincidunt elementum sapien eget, interdum ultricies
+              urna. Curabitur vel quam ut odio sodales ullamcorper at eget
+              tortor. Duis ac bibendum sem, sollicitudin aliquet nisl.
+              Suspendisse fermentum metus et nulla varius, commodo luctus eros
+              imperdiet. Fusce vitae lobortis nisi. Sed accumsan aliquam purus,
+              vitae lobortis mi scelerisque et. Nullam gravida mi a ante
+              posuere, in consequat nulla semper. Suspendisse vel porta mauris.
+              Ut eu magna venenatis, rhoncus massa in, porttitor justo. Integer
+              sit amet eros dolor. Integer pulvinar ultricies augue, vitae
+              tristique neque condimentum eget. Nam nec varius odio.
+            </p>
+          </div>
+          <div>
+            <h2>What drives me?</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              sapien mauris, tincidunt elementum sapien eget, interdum ultricies
+              urna. Curabitur vel quam ut odio sodales ullamcorper at eget
+              tortor. Duis ac bibendum sem, sollicitudin aliquet nisl.
+              Suspendisse fermentum metus et nulla varius, commodo luctus eros
+              imperdiet. Fusce vitae lobortis nisi. Sed accumsan aliquam purus,
+              vitae lobortis mi scelerisque et. Nullam gravida mi a ante
+              posuere, in consequat nulla semper. Suspendisse vel porta mauris.
+              Ut eu magna venenatis, rhoncus massa in, porttitor justo. Integer
+              sit amet eros dolor. Integer pulvinar ultricies augue, vitae
+              tristique neque condimentum eget. Nam nec varius odio.
+            </p>
+          </div>
+          <div>
+            <h2>Where am I heading?</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              sapien mauris, tincidunt elementum sapien eget, interdum ultricies
+              urna. Curabitur vel quam ut odio sodales ullamcorper at eget
+              tortor. Duis ac bibendum sem, sollicitudin aliquet nisl.
+              Suspendisse fermentum metus et nulla varius, commodo luctus eros
+              imperdiet. Fusce vitae lobortis nisi. Sed accumsan aliquam purus,
+              vitae lobortis mi scelerisque et. Nullam gravida mi a ante
+              posuere, in consequat nulla semper. Suspendisse vel porta mauris.
+              Ut eu magna venenatis, rhoncus massa in, porttitor justo. Integer
+              sit amet eros dolor. Integer pulvinar ultricies augue, vitae
+              tristique neque condimentum eget. Nam nec varius odio.
+            </p>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default About
