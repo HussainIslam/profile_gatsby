6ee5af1b9a97ecb5253262b004bb28b746c28d6f
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-05-09 16:23:42 -0400

    styled portfolio page for desktop

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,7 +3,7 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Placeholder from "../images/placeholder.png"
+import Placeholder from "../images/portfolio_images/portfolio_realtor.png"
 
 import PortStyle from "./page-css/portfolio.module.css"
 
@@ -12,20 +12,28 @@ const Portfolio = ({ data }) => {
   return (
     <Layout>
       <h2>My Works</h2>
+      <br />
+      <br />
+      <br />
       {data.allPortfolioCsv.nodes.map((item, index) => {
         return (
           <div key={index} className={PortStyle.mainContainer}>
-            <img
-              src={Placeholder}
-              alt="placeholder"
-              className={PortStyle.imageContainer}
-            />
-            <div className={PortStyle.buttonsContainer}>
-              <div className={PortStyle.singleButton}>
-                <a href={item.github}>Github</a>
-              </div>
-              <div className={PortStyle.singleButton}>
-                <a href={item.live}>Live</a>
+            <div className={PortStyle.imageContainer}>
+              <img
+                src={Placeholder}
+                alt="placeholder"
+                className={PortStyle.imageTag}
+              />
+            </div>
+            <div className={PortStyle.infoContainer}>
+              <p className={PortStyle.itemDescription}>{item.description}</p>
+              <div className={PortStyle.buttonsContainer}>
+                <div className={PortStyle.singleButton}>
+                  <a href={item.github}>Github</a>
+                </div>
+                <div className={PortStyle.singleButton}>
+                  <a href={item.live}>Live</a>
+                </div>
               </div>
             </div>
           </div>
@@ -43,6 +51,7 @@ export const query = graphql`
         live
         github
         screenshot
+        description
       }
     }
   }
