a2830f719e143fa4e242b90f0220628abaae606f
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-05-09 07:36:11 -0400

    implemented responsive navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,37 +1,146 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
-
-import { useSiteMetadata } from "../hooks/use-site-metadata"
 
 import navbarStyles from "./navbar.module.css"
 
-const NavBar = () => {
-  return (
-    <div className={navbarStyles.container}>
-      <div className={navbarStyles.brandContainer}>
-        <Link className={navbarStyles.brand} to="/">
-          {useSiteMetadata().title}
-        </Link>
+class NavBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isMenuOpen: false,
+    }
+  }
+
+  handleDisplayClick = event => {
+    this.setState({ isMenuOpen: !this.state.isMenuOpen }, () =>
+      console.log(this.state.isMenuOpen)
+    )
+  }
+
+  render() {
+    return (
+      <div className={navbarStyles.container}>
+        <div className={navbarStyles.brandContainer}>
+          <Link
+            className={`${
+              this.state.isMenuOpen
+                ? navbarStyles.hideElement
+                : navbarStyles.brand
+            }`}
+            to="/"
+          >
+            I am Hussain
+          </Link>
+        </div>
+        <div
+          className={`${
+            this.state.isMenuOpen
+              ? navbarStyles.modifiedLinks
+              : navbarStyles.links
+          }`}
+        >
+          <div
+            className={`${
+              this.state.isMenuOpen ? navbarStyles.hideElement : null
+            } ${navbarStyles.displayButtonContainer}`}
+          >
+            <button
+              className={`${
+                this.state.isMenuOpen ? navbarStyles.hideElement : null
+              } ${navbarStyles.displayButton}`}
+              onClick={this.handleDisplayClick}
+            >
+              &equiv;
+            </button>
+          </div>
+          <div
+            className={`${
+              this.state.isMenuOpen ? null : navbarStyles.hideElement
+            } ${navbarStyles.closeButtonContainer}`}
+          >
+            <button
+              className={`${
+                this.state.isMenuOpen ? null : navbarStyles.hideElement
+              } ${navbarStyles.closeButton}`}
+              onClick={this.handleDisplayClick}
+            >
+              &#8904;
+            </button>
+          </div>
+          <div
+            className={`${
+              this.state.isMenuOpen ? null : navbarStyles.hideElement
+            } ${navbarStyles.listItemContainer}`}
+          >
+            <Link
+              className={`${
+                this.state.isMenuOpen ? null : navbarStyles.hideElement
+              }  ${navbarStyles.listitem}`}
+              to="/about/"
+            >
+              About
+            </Link>
+          </div>
+          <div
+            className={`${
+              this.state.isMenuOpen ? null : navbarStyles.hideElement
+            } ${navbarStyles.listItemContainer}`}
+          >
+            <Link
+              className={`${
+                this.state.isMenuOpen ? null : navbarStyles.hideElement
+              } ${navbarStyles.listitem}`}
+              to="/portfolio/"
+            >
+              Portfolio
+            </Link>
+          </div>
+          <div
+            className={`${
+              this.state.isMenuOpen ? null : navbarStyles.hideElement
+            } ${navbarStyles.listItemContainer}`}
+          >
+            <Link
+              className={`${
+                this.state.isMenuOpen ? null : navbarStyles.hideElement
+              } ${navbarStyles.listitem}`}
+              to="/resume/"
+            >
+              Resume
+            </Link>
+          </div>
+          <div
+            className={`${
+              this.state.isMenuOpen ? null : navbarStyles.hideElement
+            } ${navbarStyles.listItemContainer}`}
+          >
+            <Link
+              className={`${
+                this.state.isMenuOpen ? null : navbarStyles.hideElement
+              } ${navbarStyles.listitem}`}
+              to="/blog/"
+            >
+              Blog
+            </Link>
+          </div>
+          <div
+            className={`${
+              this.state.isMenuOpen ? null : navbarStyles.hideElement
+            } ${navbarStyles.listItemContainer}`}
+          >
+            <Link
+              className={`${
+                this.state.isMenuOpen ? null : navbarStyles.hideElement
+              } ${navbarStyles.listitem}`}
+              to="/contact/"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className={navbarStyles.unorderedlist}>
-        <Link className={navbarStyles.listitem} to="/about/">
-          About
-        </Link>
-        <Link className={navbarStyles.listitem} to="/portfolio/">
-          Portfolio
-        </Link>
-        <Link className={navbarStyles.listitem} to="/resume/">
-          Resume
-        </Link>
-        <Link className={navbarStyles.listitem} to="/blog/">
-          Blog
-        </Link>
-        <Link className={navbarStyles.listitem} to="/contact/">
-          Contact
-        </Link>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default NavBar
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,17 +5,29 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Placeholder from "../images/placeholder.png"
 
+import PortStyle from "./page-css/portfolio.module.css"
+
 const Portfolio = ({ data }) => {
   console.log(data.allPortfolioCsv.nodes)
   return (
     <Layout>
-      <h2>This is portfolio page</h2>
+      <h2>My Works</h2>
       {data.allPortfolioCsv.nodes.map((item, index) => {
         return (
-          <div key={index}>
-            <img src={Placeholder} alt="placeholder" />
-            <a href={item.github}>Github</a>
-            <a href={item.live}>Live</a>
+          <div key={index} className={PortStyle.mainContainer}>
+            <img
+              src={Placeholder}
+              alt="placeholder"
+              className={PortStyle.imageContainer}
+            />
+            <div className={PortStyle.buttonsContainer}>
+              <div className={PortStyle.singleButton}>
+                <a href={item.github}>Github</a>
+              </div>
+              <div className={PortStyle.singleButton}>
+                <a href={item.live}>Live</a>
+              </div>
+            </div>
           </div>
         )
       })}
