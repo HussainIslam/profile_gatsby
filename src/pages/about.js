1d2862c6d40aaa522015e35af527d962e44ffa86
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-05-09 22:30:07 -0400

    added expand button in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,8 @@ import React, { Component } from "react"
 
 import Layout from "../components/layout"
 
+import AboutStyle from "./page-css/about.module.css"
+
 class About extends Component {
   constructor(props) {
     super(props)
@@ -14,14 +16,45 @@ class About extends Component {
     }
   }
 
+  handleWho = target => {
+    this.setState({ whoAmIOpen: !this.state.whoAmIOpen }, () =>
+      console.log(target)
+    )
+  }
+  handleWhereWas = target => {
+    this.setState({ whereWasIOpen: !this.state.whereWasIOpen }, () =>
+      console.log(target)
+    )
+  }
+  handleIntrigues = target => {
+    this.setState({ intriguesOpen: !this.state.intriguesOpen }, () =>
+      console.log(target)
+    )
+  }
+  handleDrives = target => {
+    this.setState({ drivesOpen: !this.state.drivesOpen }, () =>
+      console.log(target)
+    )
+  }
+  handleHeading = target => {
+    this.setState({ headingOpen: !this.state.headingOpen }, () =>
+      console.log(target)
+    )
+  }
+
   render() {
     return (
       <Layout>
         <h1>About</h1>
-        <div>
-          <div>
-            <h2>Who am I?</h2>
-            <p>
+        <div className={AboutStyle.mainContainer}>
+          <div className={AboutStyle.sectionContainer}>
+            <div className={AboutStyle.sectionHeaderContainer}>
+              <h2 className={AboutStyle.sectionHeader}>Who am I?</h2>
+              <span className={AboutStyle.openButton} onClick={this.handleWho}>
+                {this.state.whoAmIOpen ? <span>&and;</span> : <span>&or;</span>}
+              </span>
+            </div>
+            <p className={AboutStyle.sectionText}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
               sapien mauris, tincidunt elementum sapien eget, interdum ultricies
               urna. Curabitur vel quam ut odio sodales ullamcorper at eget
@@ -35,9 +68,21 @@ class About extends Component {
               tristique neque condimentum eget. Nam nec varius odio.
             </p>
           </div>
-          <div>
-            <h2>Where was I?</h2>
-            <p>
+          <div className={AboutStyle.sectionContainer}>
+            <div className={AboutStyle.sectionHeaderContainer}>
+              <h2 className={AboutStyle.sectionHeader}>Where was I?</h2>
+              <span
+                className={AboutStyle.openButton}
+                onClick={this.handleWhereWas}
+              >
+                {this.state.whereWasIOpen ? (
+                  <span>&and;</span>
+                ) : (
+                  <span>&or;</span>
+                )}
+              </span>
+            </div>
+            <p className={AboutStyle.sectionText}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
               sapien mauris, tincidunt elementum sapien eget, interdum ultricies
               urna. Curabitur vel quam ut odio sodales ullamcorper at eget
@@ -51,9 +96,21 @@ class About extends Component {
               tristique neque condimentum eget. Nam nec varius odio.
             </p>
           </div>
-          <div>
-            <h2>What intrigues me?</h2>
-            <p>
+          <div className={AboutStyle.sectionContainer}>
+            <div className={AboutStyle.sectionHeaderContainer}>
+              <h2 className={AboutStyle.sectionHeader}>What intrigues me?</h2>
+              <span
+                className={AboutStyle.openButton}
+                onClick={this.handleIntrigues}
+              >
+                {this.state.intriguesOpen ? (
+                  <span>&and;</span>
+                ) : (
+                  <span>&or;</span>
+                )}
+              </span>
+            </div>
+            <p className={AboutStyle.sectionText}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
               sapien mauris, tincidunt elementum sapien eget, interdum ultricies
               urna. Curabitur vel quam ut odio sodales ullamcorper at eget
@@ -67,9 +124,17 @@ class About extends Component {
               tristique neque condimentum eget. Nam nec varius odio.
             </p>
           </div>
-          <div>
-            <h2>What drives me?</h2>
-            <p>
+          <div className={AboutStyle.sectionContainer}>
+            <div className={AboutStyle.sectionHeaderContainer}>
+              <h2 className={AboutStyle.sectionHeader}>What drives me?</h2>
+              <span
+                className={AboutStyle.openButton}
+                onClick={this.handleDrives}
+              >
+                {this.state.drivesOpen ? <span>&and;</span> : <span>&or;</span>}
+              </span>
+            </div>
+            <p className={AboutStyle.sectionText}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
               sapien mauris, tincidunt elementum sapien eget, interdum ultricies
               urna. Curabitur vel quam ut odio sodales ullamcorper at eget
@@ -83,9 +148,21 @@ class About extends Component {
               tristique neque condimentum eget. Nam nec varius odio.
             </p>
           </div>
-          <div>
-            <h2>Where am I heading?</h2>
-            <p>
+          <div className={AboutStyle.sectionContainer}>
+            <div className={AboutStyle.sectionHeaderContainer}>
+              <h2 className={AboutStyle.sectionHeader}>Where am I heading?</h2>
+              <span
+                className={AboutStyle.openButton}
+                onClick={this.handleHeading}
+              >
+                {this.state.headingOpen ? (
+                  <span>&and;</span>
+                ) : (
+                  <span>&or;</span>
+                )}
+              </span>
+            </div>
+            <p className={AboutStyle.sectionText}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
               sapien mauris, tincidunt elementum sapien eget, interdum ultricies
               urna. Curabitur vel quam ut odio sodales ullamcorper at eget
