d15fcc591def1168598a2bbf4210f8c462915dd9
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-08 10:28:16 -0500

    refactore navigation buttons to NavButton component

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
-//import AniLink from "gatsby-plugin-transition-link/Link"
+import NavButton from '../navbutton/navbutton'
 
 import navbarStyles from "./navbar.module.css"
 
@@ -14,125 +14,23 @@ const NavBar = () =>{
   return (
     /* Main Container */
     <div className={navbarStyles.container}>
-      <div
-        className={`${
-          isMenuOpen
-            ? navbarStyles.modifiedLinks
-            : navbarStyles.links
-        }`}
-      >
-        <div
-          className={`${
-            isMenuOpen ? navbarStyles.hideElement : null
-          } ${navbarStyles.displayButtonContainer}`}
-        >
-          <button
-            className={`${
-              isMenuOpen ? navbarStyles.hideElement : null
-            } ${navbarStyles.displayButton}`}
-            onClick={handleDisplayClick}
-          >
+      <div className={`${isMenuOpen ? navbarStyles.modifiedLinks: navbarStyles.links}`}>
+        <div className={`${isMenuOpen ? navbarStyles.hideElement : null} ${navbarStyles.displayButtonContainer}`}>
+          <button className={`${isMenuOpen ? navbarStyles.hideElement : null} ${navbarStyles.displayButton}`}onClick={handleDisplayClick}>
             &equiv;
           </button>
         </div>
-        <div
-          className={`${
-            isMenuOpen ? null : navbarStyles.hideElement
-          } ${navbarStyles.closeButtonContainer}`}
-        >
-          <button
-            className={`${
-              isMenuOpen ? null : navbarStyles.hideElement
-            } ${navbarStyles.closeButton}`}
-            onClick={handleDisplayClick}
-          >
+        <div className={`${isMenuOpen ? null : navbarStyles.hideElement} ${navbarStyles.closeButtonContainer}`}>
+          <button className={`${isMenuOpen ? null : navbarStyles.hideElement} ${navbarStyles.closeButton}`}onClick={handleDisplayClick}>
             X
           </button>
         </div>
-        <div
-          className={`${navbarStyles.listItemContainer} ${
-            isMenuOpen ? null : navbarStyles.hideElement
-          }`}
-        >
-          <Link
-            className={`${
-              isMenuOpen ? null : navbarStyles.hideElement
-            }  ${navbarStyles.listItem}`}
-            to="/"
-          >
-            Home
-          </Link>
-        </div>
-        <div
-          className={`${
-            isMenuOpen ? null : navbarStyles.hideElement
-          } ${navbarStyles.listItemContainer}`}
-        >
-          <Link
-            className={`${
-              isMenuOpen ? null : navbarStyles.hideElement
-            }  ${navbarStyles.listItem}`}
-            to="/about/"
-          >
-            About
-          </Link>
-        </div>
-        <div
-          className={`${
-            isMenuOpen ? null : navbarStyles.hideElement
-          } ${navbarStyles.listItemContainer}`}
-        >
-          <Link
-            className={`${
-              isMenuOpen ? null : navbarStyles.hideElement
-            } ${navbarStyles.listItem}`}
-            to="/portfolio/"
-          >
-            Portfolio
-          </Link>
-        </div>
-        <div
-          className={`${
-            isMenuOpen ? null : navbarStyles.hideElement
-          } ${navbarStyles.listItemContainer}`}
-        >
-          <Link
-            className={`${
-              isMenuOpen ? null : navbarStyles.hideElement
-            } ${navbarStyles.listItem}`}
-            to="/resume/"
-          >
-            Resume
-          </Link>
-        </div>
-        <div
-          className={`${
-            isMenuOpen ? null : navbarStyles.hideElement
-          } ${navbarStyles.listItemContainer}`}
-        >
-          <Link
-            className={`${
-              isMenuOpen ? null : navbarStyles.hideElement
-            } ${navbarStyles.listItem}`}
-            to="/blog/"
-          >
-            Blog
-          </Link>
-        </div>
-        <div
-          className={`${
-            isMenuOpen ? null : navbarStyles.hideElement
-          } ${navbarStyles.listItemContainer}`}
-        >
-          <Link
-            className={`${
-              isMenuOpen ? null : navbarStyles.hideElement
-            } ${navbarStyles.listItem}`}
-            to="/contact/"
-          >
-            Contact
-          </Link>
-        </div>
+        <NavButton text="Home" isMenuOpen={isMenuOpen} />
+        <NavButton text="About" isMenuOpen={isMenuOpen} />
+        <NavButton text="Portfolio" isMenuOpen={isMenuOpen} />
+        <NavButton text="Resume" isMenuOpen={isMenuOpen} />
+        <NavButton text="Blog" isMenuOpen={isMenuOpen} />
+        <NavButton text="Contact" isMenuOpen={isMenuOpen} />
       </div>
     </div>
   )
---
+++ src/components/navbutton/navbutton.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+import Styles from './navbutton.module.css'
+
+const NavButton = ({ isMenuOpen, text }) => {
+    const toValue = text == 'Home' ? '/' : text.toLowerCase();
+    return (
+        <div className={`${Styles.listItemContainer} ${isMenuOpen ? null : Styles.hideElement}`}>
+          <Link className={`${isMenuOpen ? null : Styles.hideElement}  ${Styles.listItem}`}to={toValue} >
+            {text}
+          </Link>
+        </div>
+    )
+}
+
+export default NavButton
