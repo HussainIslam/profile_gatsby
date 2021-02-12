2ba8283332e1661416f821480f6f232213ca6251
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-12 12:00:51 -0500

    tech-icon background color to white

--- src/components/tech-icons/tech-icons.js
+++ src/components/tech-icons/tech-icons.js
@@ -8,26 +8,24 @@ import Topic from "../topic"
 const TechIcons = () => {
   const data = useTechIcons()
   return (
-    <div className={TI.mainContainer}>
-      <div className={TI.iconContainer}>
-        {data.map((item, index) => {
-          const titleName = item.node.name.substring(5, item.node.name.length)
-          return (
-            <div key={item.node.id} className={TI.iconDiv}>
-              <img
-                className={TI.icon}
-                src={item.node.publicURL}
-                alt="technology icon"
-                title={titleName}
-              />
-              <p className={TI.iconName}>
-                {titleName[0].toUpperCase() +
-                  titleName.substring(1, titleName.length)}
-              </p>
-            </div>
-          )
-        })}
-      </div>
+    <div className={TI.iconContainer}>
+      {data.map((item, index) => {
+        const titleName = item.node.name.substring(5, item.node.name.length)
+        return (
+          <div key={item.node.id} className={TI.iconDiv}>
+            <img
+              className={TI.icon}
+              src={item.node.publicURL}
+              alt="technology icon"
+              title={titleName}
+            />
+            <p className={TI.iconName}>
+              {titleName[0].toUpperCase() +
+                titleName.substring(1, titleName.length)}
+            </p>
+          </div>
+        )
+      })}
     </div>
   )
 }
