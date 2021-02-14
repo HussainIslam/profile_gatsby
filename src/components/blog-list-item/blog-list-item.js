b1ef2a1f21c6fef4d4b5ca7dc60bb2c6210ad4b4
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-14 13:33:34 -0500

    refac blog list item

---
+++ src/components/blog-list-item/blog-list-item.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
+import BlogStyle from './blog-list-item.module.css'
+
+const BlogListItem = ({ node }) => {
+    return (
+        <div key={node.id} className={BlogStyle.blogContainer}>
+            <Link to={node.fields.slug} className={BlogStyle.blogLinks}>
+                <div className={BlogStyle.blogListContainer}>
+                <div className={BlogStyle.blogListImage}>
+                    <Img fixed={node.frontmatter.featuredImage.childImageSharp.fixed} />
+                </div>
+                <div>
+                    <h3 className={BlogStyle.blogTitle}>{node.frontmatter.title}</h3>{" "}
+                    <h4 className={BlogStyle.blogPostDate}>
+                    Posted: {node.frontmatter.date}
+                    </h4>
+                    <p>{node.excerpt}</p>
+                </div>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default BlogListItem
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Img from 'gatsby-image'
 import LatestBlog from '../components/latest-blog/latest-blog'
+import BlogListItem from '../components/blog-list-item/blog-list-item'
 import BlogStyle from "./page-css/blog.module.css"
 
 const Blog = ({ data }) => {
@@ -15,24 +16,7 @@ const Blog = ({ data }) => {
     <SEO title="Blog" />
     {data.allMarkdownRemark.edges.map(( {node}, index ) => 
       (index === 0?
-        <LatestBlog node={node} />:
-        (<div key={node.id} className={BlogStyle.blogContainer}>
-          <Link to={node.fields.slug} className={BlogStyle.blogLinks}>
-            <div className={BlogStyle.blogListContainer}>
-              <div className={BlogStyle.blogListImage}>
-                <Img fixed={node.frontmatter.featuredImage.childImageSharp.fixed} />
-              </div>
-              <div>
-                <h3 className={BlogStyle.blogTitle}>{node.frontmatter.title}</h3>{" "}
-                <h4 className={BlogStyle.blogPostDate}>
-                  Posted: {node.frontmatter.date}
-                </h4>
-                <p>{node.excerpt}</p>
-
-              </div>
-            </div>
-          </Link>
-        </div>)
+        <LatestBlog node={node} />: <BlogListItem node={node}/>
       )
     )}
   </Layout>
